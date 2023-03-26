d17d810ca5fa6dd5a1182fff19e9051b4890f784
Waliyu Habeeb <Ademolawaliyu1@gmail.com>
2023-03-26 03:55:58 -0700

    blogweb

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+
+const bodyparser = require("body-parser")
+
+
+
+const PORT = process.env.PORT || 1000;
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(express.static("public"));
+app.set("veiw engine", "ejs");
+
+app.get('/landing', (req, res, next) => {
+    res.render("landing.ejs")
+});
+
+
+
+
+
+
+
+app.use((req, res, next) => {
+    res.status(200).json ({
+        message: "hello its working"
+    });
+});
+
+app.use((req, res, next) => {
+    const error = new error ("not available")
+    error.status = 404
+    next(error)
+});
+
+
+
+
+
+app.listen(PORT, () => console.log ("server listening on port " + PORT))
